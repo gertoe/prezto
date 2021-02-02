a170bca44177bc8b3dd66f50a6eca9b59a183377
Gerrit <gerrittoehgiono@googlemail.com>
2021-02-02 14:29:44 +0100

    Disable noglob and autocorrect on utilities, remove ls aliases

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -20,32 +20,32 @@ fi
 #
 
 # Disable correction.
-alias ack='nocorrect ack'
-alias cd='nocorrect cd'
-alias cp='nocorrect cp'
-alias ebuild='nocorrect ebuild'
-alias gcc='nocorrect gcc'
-alias gist='nocorrect gist'
-alias grep='nocorrect grep'
-alias heroku='nocorrect heroku'
-alias ln='nocorrect ln'
-alias man='nocorrect man'
-alias mkdir='nocorrect mkdir'
-alias mv='nocorrect mv'
-alias mysql='nocorrect mysql'
-alias rm='nocorrect rm'
+# alias ack='nocorrect ack'
+# alias cd='nocorrect cd'
+# alias cp='nocorrect cp'
+# alias ebuild='nocorrect ebuild'
+# alias gcc='nocorrect gcc'
+# alias gist='nocorrect gist'
+# alias grep='nocorrect grep'
+# alias heroku='nocorrect heroku'
+# alias ln='nocorrect ln'
+# alias man='nocorrect man'
+# alias mkdir='nocorrect mkdir'
+# alias mv='nocorrect mv'
+# alias mysql='nocorrect mysql'
+# alias rm='nocorrect rm'
 
 # Disable globbing.
-alias bower='noglob bower'
-alias fc='noglob fc'
-alias find='noglob find'
-alias ftp='noglob ftp'
-alias history='noglob history'
-alias locate='noglob locate'
-alias rake='noglob rake'
-alias rsync='noglob rsync'
-alias scp='noglob scp'
-alias sftp='noglob sftp'
+# alias bower='noglob bower'
+# alias fc='noglob fc'
+# alias find='noglob find'
+# alias ftp='noglob ftp'
+# alias history='noglob history'
+# alias locate='noglob locate'
+# alias rake='noglob rake'
+# alias rsync='noglob rsync'
+# alias scp='noglob scp'
+# alias sftp='noglob sftp'
 
 # Define general aliases.
 alias _='sudo'
@@ -113,17 +113,17 @@ else
   fi
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # I often screw this up.
+# alias l='ls -1A'         # Lists in one column, hidden files.
+# alias ll='ls -lh'        # Lists human readable sizes.
+# alias lr='ll -R'         # Lists human readable sizes, recursively.
+# alias la='ll -A'         # Lists human readable sizes, hidden files.
+# alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
+# alias lx='ll -XB'        # Lists sorted by extension (GNU only).
+# alias lk='ll -Sr'        # Lists sorted by size, largest last.
+# alias lt='ll -tr'        # Lists sorted by date, most recent last.
+# alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
+# alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
+#alias sl='ls'            # I often screw this up.
 
 # Grep
 if zstyle -t ':prezto:module:utility:grep' color; then
